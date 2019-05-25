0a6067a57debd34be688571fb720e60cf612eee4
roopabdr <roopabdr@gmail.com>
2019-05-25 12:58:05 +0530

    created initial app for generating XSD with abilities to download xsd file to local storage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    element_field: [],
+    length_field: [],
+    element_texarea: '',
+    length_textarea: ''
+  }
+  
+  generateXSD = () => {
+    console.log(this.state.element_field);
+    this.downloadTxtFile();
+  }
+
+  downloadTxtFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob([this.state.element_field], {type: 'text/xml'});
+    element.href = URL.createObjectURL(file);
+    element.download = "myFile.xsd";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  }
+
+  onDataInputChange = (event) => {
+    this.setState({element_texarea: event.target.value});
+  }
+
+  onGenerateClick = () => {
+    let elem_value = this.state.element_texarea.replace(/^\s*$(?:\r\n?|\n)/gm,'').trim().split('\n');
+    elem_value = elem_value.map((item, index) => 'f' + (index + 1).toString().concat('_',item.replace(/ /g,'_')));
+    this.setState({element_field: elem_value}, function() {
+      this.generateXSD();
+    });    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Generate Damn XSD for CNB</h1>
+          <button className="success" onClick={this.onGenerateClick}>Generate</button>
+        </header>
+        <div className='fields'>
+          <div className='fields_area'>
+            <h2>Copy/Paste field names here:</h2>
+            <textarea name="fields" id="fields_id" cols="40" rows="30" onChange={this.onDataInputChange}></textarea>
+          </div>
+          <div className='length_area'>
+            <h2>Copy/Paste field lenghts here: </h2>
+            <textarea name="fields" id="fields_id" cols="20" rows="30"></textarea>
+          </div>
+        </div>      
+      </div>
+    )
+  }
 }
 
 export default App;
