f2700d17b2f2ac237e661cf5dc630136b4a070eb
roopabdr <roopabdr@gmail.com>
2019-05-25 13:03:46 +0530

    Updates

---
+++ precache-manifest.73605c06606ad5b590bc1ce844382e6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a4499448c8cfb6816bcb4077cd7207",
+    "url": "/cnb-generate-damn-xsd/index.html"
+  },
+  {
+    "revision": "32272d6ffdfb027fe408",
+    "url": "/cnb-generate-damn-xsd/static/css/main.b96c2bf5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/cnb-generate-damn-xsd/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "32272d6ffdfb027fe408",
+    "url": "/cnb-generate-damn-xsd/static/js/main.5e7fd104.chunk.js"
+  },
+  {
+    "revision": "2b6d61bbf61cc3b6242d",
+    "url": "/cnb-generate-damn-xsd/static/js/runtime~main.223bb43f.js"
+  }
+]);
